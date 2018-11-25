e5253a30b3fdbb171bc75add8724bce477009cbd
huyhoang1996ha <huyhoang1996ha@gmail.com>
2018-11-25 16:48:41 +0700

    add documenting api

--- websites/main/urls.py
+++ websites/main/urls.py
@@ -17,11 +17,23 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+# from rest_framework_swagger.views import get_swagger_view
+# schema_view2 = get_swagger_view(title='Swagger API')
+
+schema_view = get_schema_view(title="Example API")
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^admin/', admin.site.urls),
-    url(r'api/', include('app.urls')),
-    url(r'^api-auth/', include('rest_framework.urls'))
+    url(r'^api/', include('app.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^docs/', include_docs_urls(title='API Documente'))
+
+    # url(r'^docs2/$', schema_view2),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += urlpatterns + static(settings.OUTSIDE_URL, document_root=settings.OUTSIDE_ROOT)
